c09a528f2ecd3629de10199634e2c1a0b8927377
Ilya Lisin <blacklacost@gmail.com>
2020-06-12 13:49:21 +0300

    QC: загругка категорий

--- src/models/categories.py
+++ src/models/categories.py
@@ -16,20 +16,12 @@ class Categories:
         self._categories = self._load_categories()
 
     def _load_categories(self) -> List[Category]:
-        with connect() as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                    "SELECT codename, name, is_base_expense FROM category"
-                )
-                categories = [
-                    Category(
-                        codename=codename,
-                        name=name,
-                        is_base_expense=is_base_expense,
-                    )
-                    for (codename, name, is_base_expense) in cur
-                ]
-                return categories
+        with connect() as conn, conn.cursor() as cur:
+            cur.execute(
+                "SELECT codename, name, is_base_expense FROM category"
+            )
+            categories = [Category(*row) for row in cur]
+            return categories
 
     def get_all_categories(self) -> List[Category]:
         return self._categories
