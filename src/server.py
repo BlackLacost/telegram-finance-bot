8fb7633c4c7e3cf3a345cbff07a3569e43e505de
Ilya Lisin <blacklacost@gmail.com>
2020-06-12 10:09:26 +0300

    Ограничение доступа по user_id

--- src/server.py
+++ src/server.py
@@ -5,15 +5,20 @@ from pathlib import Path
 from aiogram import Bot, Dispatcher, types, executor
 from dotenv import load_dotenv
 
+from middlewares import AccessMiddleware
+
 load_dotenv(dotenv_path=Path(".") / ".env")
 
 API_TOKEN = os.getenv("API_TOKEN")
 PROXY_URL = os.getenv("PROXY_URL")
+ACCESS_ID = os.getenv("ACCESS_ID")
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN, proxy=PROXY_URL)
 dp = Dispatcher(bot)
+if ACCESS_ID:
+    dp.middleware.setup(AccessMiddleware(int(ACCESS_ID)))
 
 
 @dp.message_handler(commands=["start", "help"])
