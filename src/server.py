71fb98c4497c5b84cab61a55cfd142307b48cc5d
Ilya Lisin <blacklacost@gmail.com>
2020-06-13 06:23:28 +0300

    QC: переименовал controllers to handlers

--- src/server.py
+++ src/server.py
@@ -2,15 +2,11 @@ import logging
 
 from aiogram import executor
 
-import controllers
+import handlers
 from init_bot import dp
 from models.db import init_db_if_not_exists
 
 
-logging.basicConfig(level=logging.INFO)
-
 init_db_if_not_exists()
-
-
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+logging.basicConfig(level=logging.INFO)
+executor.start_polling(dp, skip_updates=True)
